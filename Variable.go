615f35da90084b073a6fa943c9551ad48f0de527
pmp2601 <83898154+pmp2601@users.noreply.github.com>
2025-12-19 19:30:26 +0530

    add const file

--- Variable.go
+++ Variable.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+const Pi = 3.14
+const Greeting = "Hello, Go!"
+
 func main() {
 	fmt.Println("Hello, World!")
+	const days int = 7
+
+	const (
+		Monday        = 1
+		Tuesday       = 2
+		Wednesday     = 3
+		Thursday      = 4
+		Friday        = 5
+		Saturday  int = 6
+	)
+	//name := "Alice" // not declared using shotend
 }
