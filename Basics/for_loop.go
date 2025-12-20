883b6205f8d0f9f009f43b86b6f4a2d88746eef3
pmp2601 <83898154+pmp2601@users.noreply.github.com>
2025-12-20 17:43:01 +0530

    add variable and for_loop

--- Basics/for_loop.go
+++ Basics/for_loop.go
@@ -1,9 +1,50 @@
-package main
+package Basics
 
 import "fmt"
 
 func main() {
+
+	//simple for loop
 	for i := 0; i < 5; i++ {
 		fmt.Println("Iteration:", i)
 	}
+
+	//ierating over a slice
+	numbers := []int{10, 20, 30, 40, 50}
+	for index, value := range numbers {
+		fmt.Printf("Index: %d, Value: %d\n", index, value)
+	}
+
+	//break and continue
+
+	for i := 0; i <= 5; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("Odd Number:", i)
+		if i == 5 {
+			break
+		}
+	}
+
+	//Printing stars in a pattern
+	rows := 5
+	// Outer loop for each row
+	for i := 1; i <= rows; i++ {
+		//print spaces in inner loop
+		for j := 1; j <= rows-i; j++ {
+			fmt.Print(" ")
+		}
+		//inner loop for stars
+		for k := 1; k <= 2*i-1; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	for i := range 10 {
+		i++
+		fmt.Println(i)
+	}
+
 }
